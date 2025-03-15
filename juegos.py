15e946f589429d27a70ab48a517865cad18a7475
JuanQ-04 <jquinone11@cuc.edu.co>
2025-03-15 14:17:58 -0500

    Se crea el archivo

---
+++ juegos.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+class PiedraPapelTijeras():
+
+
+    def opcionesDelJuego(opcion):
+        if opcion == 1:
+            return "Piedra"
+        elif opcion == 2:
+            return "Papel"
+        elif opcion == 3:
+            return "Tijeras"
