cdc27c3fab9d2090d5985142c1bfb2aae8071e2e
JuanQ-04 <jquinone11@cuc.edu.co>
2025-03-15 14:21:13 -0500

    Se crea la funcion para que inicie el juego junto con las condicionales de victoria, derrota y empate

--- juegos.py
+++ juegos.py
@@ -27,3 +27,29 @@ class PiedraPapelTijeras():
                     print( f"El valor '{opcion}' es invalido")
             except ValueError:
                 print("Por favor, ingresa un número válido.")
+
+    def juego():
+
+        opcion_jugador = PiedraPapelTijeras.opcionJugador()
+        opcion_maquina = PiedraPapelTijeras.opcionMaquina()
+
+        jugador = PiedraPapelTijeras.opcionesDelJuego(opcion_jugador)
+        maquina = PiedraPapelTijeras.opcionesDelJuego(opcion_maquina)
+
+        print("-"*15)
+        print("Calculando el resultado....")
+        print("-"*15)
+        time.sleep(0.3)
+
+        resultado = ""
+        if jugador == maquina:
+            resultado = f"Tu elección: {jugador}\nElección de la máquina: {maquina}\n\n¡Es un empate!"
+        elif (jugador == "Piedra" and maquina == "Tijeras") or (jugador == "Papel" and maquina == "Piedra") or (jugador == "Tijeras" and maquina == "Papel"):
+            resultado = f"Tu elección: {jugador}\nElección de la máquina: {maquina}\n¡Ganaste!"
+        else:
+            resultado = f"Tu elección: {jugador}\nElección de la máquina: {maquina}\n¡Perdiste!"
+
+        print("\n")
+        print("-"*15)
+        print (resultado)
+        print("-"*15)
