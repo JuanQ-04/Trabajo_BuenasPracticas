fc4069c55436477efb7230177bd06132c6f90f1a
JuanQ-04 <jquinone11@cuc.edu.co>
2025-03-15 14:20:16 -0500

    Se crea la funcion que da la opcion del jugador y de la maquina

--- juegos.py
+++ juegos.py
@@ -12,4 +12,18 @@ class PiedraPapelTijeras():
         elif opcion == 3:
             return "Tijeras"
     
-    
+    def opcionMaquina():
+        opcion = random.randint(1,3)
+        return opcion
+
+    def opcionJugador():
+        while True:
+            print("\nOpciones de juego\nPiedra (1)\nPapel (2)\nTijeras (3)\n")
+            try:
+                opcion = int(input("Ingrese su opcion: "))
+                if opcion > 0 and opcion < 4:
+                    return opcion
+                else:
+                    print( f"El valor '{opcion}' es invalido")
+            except ValueError:
+                print("Por favor, ingresa un número válido.")
